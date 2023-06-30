8c7fa7e5dfe750b78ae4e22bb6e2073eac087bdc
Tesfay26 <tesfaybsrat26@gmail.com>
2023-06-30 11:26:10 +0300

    Producer attributes added

--- Ticket-System/Models/Producer.cs
+++ Ticket-System/Models/Producer.cs
@@ -1,6 +1,13 @@
-﻿namespace Ticket_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticket_System.Models
 {
     public class Producer
     {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public string FullName { get; set; }
+        public string Biography { get; set; }
     }
 }
