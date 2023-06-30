16774a67647d10646e6b0832c0627efcca9593c3
Tesfay26 <tesfaybsrat26@gmail.com>
2023-06-30 11:12:26 +0300

    Actor model created

---
+++ Ticket-System/Models/Actor.cs
@@ -0,0 +1,9 @@
+﻿namespace Ticket_System.Models
+{
+    public class Actor
+    {
+        public string ProfilePicture { get; set; }
+        public string FullName { get; set; }
+        public string Biography { get; set; }
+    }
+}
