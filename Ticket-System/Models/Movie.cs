f91c74f9c8669b28b87ff609b970c7b9f47f0009
Tesfay26 <tesfaybsrat26@gmail.com>
2023-06-30 11:49:47 +0300

    Movie Model and MovieCategory enm are added.

---
+++ Ticket-System/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using Ticket_System.Data;
+
+namespace Ticket_System.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategories { get; set; }
+    }
+}
