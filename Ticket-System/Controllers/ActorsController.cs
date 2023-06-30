bda3cd3517eb983ebf88439bbe49f4da0e21b317
Tesfay26 <tesfaybsrat26@gmail.com>
2023-06-30 16:59:40 +0300

    Actor Controller Added

---
+++ Ticket-System/Controllers/ActorsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ticket_System.Data;
+
+namespace Ticket_System.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDBContext _dbContext;
+        public ActorsController(AppDBContext context)
+        {
+            _dbContext = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _dbContext.Actors.ToList();
+            return View();
+        }
+    }
+}
