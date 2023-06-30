6b63b71ef0a79bedb924fd73a74a84f0a7a7e63d
Tesfay26 <tesfaybsrat26@gmail.com>
2023-06-30 17:17:25 +0300

    Movies Controller Added

---
+++ Ticket-System/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ticket_System.Data;
+
+namespace Ticket_System.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDBContext _dbContext;
+        public MoviesController(AppDBContext context)
+        {
+            _dbContext = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _dbContext.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
