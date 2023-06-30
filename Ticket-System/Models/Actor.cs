5de4edce6ebabf755ddd6197c3d22a25666ddd54
Tesfay26 <tesfaybsrat26@gmail.com>
2023-06-30 11:15:40 +0300

    Actor primary key added

--- Ticket-System/Models/Actor.cs
+++ Ticket-System/Models/Actor.cs
@@ -1,7 +1,11 @@
-﻿namespace Ticket_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticket_System.Models
 {
     public class Actor
     {
+        [Key]
+        public int Id { get; set; }
         public string ProfilePicture { get; set; }
         public string FullName { get; set; }
         public string Biography { get; set; }
